931bc144da70d0ec277d2176f599655e85f1137c
narzk <niloofararazkhani@gmai.com>
2019-12-28 11:53:23 +0330

    Controlled Form Validation

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
 import {
   Card,
   CardImg,
@@ -9,54 +10,96 @@ import {
   Row,
   Col,
   ListGroup,
-  ListGroupItem
+  ListGroupItem,
+  Breadcrumb,
+  BreadcrumbItem
 } from "reactstrap";
 
-
-  function RenderComments({comments}) {
-    {console.log(comments)}
-    const cmt = comments.map(cm => {
-      return (
-        <ListGroup>
-          <ListGroupItem key={cm.id}>
-            {cm.comment}
-            <div>--{cm.author}</div>
-            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(cm.date)))}
-            {console.log(222, cm.author)}
-          </ListGroupItem>
-        </ListGroup>
-      );
-    });
-    return cmt;
+function RenderComments({ comments }) {
+  {
+    console.log(comments);
   }
+  const cmt = comments.map(cm => {
+    return (
+      <ListGroup>
+        <ListGroupItem key={cm.id}>
+          {cm.comment}
+          <div>--{cm.author}</div>
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit"
+          }).format(new Date(Date.parse(cm.date)))}
+          {console.log(222, cm.author)}
+        </ListGroupItem>
+      </ListGroup>
+    );
+  });
+  return cmt;
+}
+function RenderDish({ dish }) {
+  return (
+    <Card>
+      <CardImg top src={dish.image} alt={dish.name} />
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
 
-  const  DishDetail = (props) => {
-    if (props.dish != null)
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-5 m-1">
-              <Card>
-                <CardImg
-                  top
-                  src={props.dish.image}
-                  alt={props.dish.name}
-                />
-                <CardBody>
-                  <CardTitle>{props.dish.name}</CardTitle>
-                  <CardText>{props.dish.description}</CardText>
-                </CardBody>
-              </Card>
-            </div>
-            <div className="col-12 col-md-5 m-1">
-              <ListGroupItem><h1 >Comments</h1></ListGroupItem>
-              <RenderComments comments={props.dish.comments}/>
-            </div>
+const DishDetail = props => {
+  if (props.dish != null)
+    return (
+      // <div className="container">
+      //   <div className="row">
+      //     <div className="col-12 col-md-5 m-1">
+      //       <Card>
+      //         <CardImg
+      //           top
+      //           src={props.dish.image}
+      //           alt={props.dish.name}
+      //         />
+      //         <CardBody>
+      //           <CardTitle>{props.dish.name}</CardTitle>
+      //           <CardText>{props.dish.description}</CardText>
+      //         </CardBody>
+      //       </Card>
+      //     </div>
+      //     <div className="col-12 col-md-5 m-1">
+      //       <ListGroupItem><h1 >Comments</h1></ListGroupItem>
+      //       <RenderComments comments={props.dish.comments}/>
+      //     </div>
+      //   </div>
+      // </div>
+
+      <div className="container">
+        
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
           </div>
         </div>
-      );
-    else return <div></div>;
-      }
-
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+          <h1>Comments</h1>
+            <RenderComments comments={props.comments} />
+          </div>
+        </div>
+      </div>
+    );
+  else return <div></div>;
+};
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
+import About from "./AboutComponent"
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
@@ -11,6 +12,8 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { COMMENTS } from "../shared/comments";
 import { PROMOTIONS } from "../shared/promotions";
 import { LEADERS } from "../shared/leaders";
+
+
 
 class Main extends Component {
   constructor(props) {
@@ -34,7 +37,12 @@ class Main extends Component {
         />
       );
     };
-
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return (
       <div>
         <Header />
@@ -47,6 +55,8 @@ class Main extends Component {
             component={() => <Menu dishes={this.state.dishes} />}
           />
           <Route exact path="/contactus" component={Contact} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
